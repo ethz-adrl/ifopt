c81bd233c89eed4b1667e50187fdaebcbcf6dba2
Vivian Suzano Medeiros <viviansuzano@hotmail.com>
2018-10-01 14:21:42 +0200

    No need to include iostream here

--- ifopt_ipopt/src/ipopt_adapter.cc
+++ ifopt_ipopt/src/ipopt_adapter.cc
@@ -24,7 +24,6 @@ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************************************************************/
 
-#include <iostream>
 #include <ifopt/ipopt_adapter.h>
 #include <ifopt/ipopt_solver.h>
 
